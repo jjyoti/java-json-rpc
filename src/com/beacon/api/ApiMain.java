1f67d40b4a6e5c81790185dab9a41db4a66bcb1f
jjyoti <jsjyotisachan@gmail.com>
2014-07-26 19:15:45 +0530

    Added api test file

---
+++ src/com/beacon/api/ApiMain.java
@@ -0,0 +1,13 @@
+package com.beacon.api;
+
+public class ApiMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test");
+	}
+
+}
