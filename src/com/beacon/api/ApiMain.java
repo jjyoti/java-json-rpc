a0760d859393ca7c2320a1d512174399a8584dc2
jjyoti <jsjyotisachan@gmail.com>
2014-07-26 19:19:00 +0530

    updated message and made it better

--- src/com/beacon/api/ApiMain.java
+++ src/com/beacon/api/ApiMain.java
@@ -7,7 +7,7 @@ public class ApiMain {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is a test");
+		System.out.println("This is a test and it is going to be successful!");
 	}
 
 }
